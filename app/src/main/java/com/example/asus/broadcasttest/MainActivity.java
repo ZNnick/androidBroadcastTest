a4f585c20301b0362a39729ee4861eeda066b525
ZN_nick <znnznick@gmail.com>
2018-03-25 15:28:09 +0800

    first

--- app/src/main/java/com/example/asus/broadcasttest/MainActivity.java
+++ app/src/main/java/com/example/asus/broadcasttest/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.asus.broadcasttest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"")
+        }
     }
 }
